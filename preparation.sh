826bf357d2fa2f198cbe6f197c83ca299ce8f2b1
Abrar Ahmed <abrargroad2000@gmail.com>
2025-08-04 12:15:43 +0600

    Removed amend

---
+++ preparation.sh
@@ -0,0 +1,7 @@
+# Clone the repo
+git clone https://github.com/abrarahmd/learn-git.git
+cd learn-git
+
+# Configure identity
+git config user.name "Abrar Ahmed"
+git config user.email "abrargroad2000@gmail.com"
